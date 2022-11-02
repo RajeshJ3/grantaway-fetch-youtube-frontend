ea83f6e53aa6751923e6a4423acc0659a60d1a1b
Rajesh Joshi <joshirajesh448@gmail.com>
2022-11-03 00:24:34 +0530

    base code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "./utils";
 
-function App() {
+export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: `${BASE_URL}/`,
+      method: "GET",
+      params: {
+        delta_hours: 24,
+      },
+    }).then((res) => {
+      setVideos(res.data.payload);
+      setLoading(false);
+    });
+  }, []);
+
+  const fetchMore = () => {
+    axios({
+      url: `${BASE_URL}/`,
+      method: "POST",
+      params: {
+        q: "BGMI Giveaway",
+        save: 1,
+      },
+    }).then(() => window.location.reload());
+  };
+
+  const refresh = (video) => {
+    window.location.reload();
+  };
+
+  const postVideo = (video) => {
+    axios({
+      url: `${BASE_URL}/post/${video.pk}`,
+      method: "GET",
+    }).then(() => alert(`"${video.video_title}" posted`));
+  };
+
+  const deleteVideo = (video) => {
+    axios({
+      url: `${BASE_URL}/${video.pk}`,
+      method: "DELETE",
+    }).then(() => alert(`"${video.video_title}" deleted`));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={refresh}>Refresh</button>
+      <button onClick={fetchMore}>Fetch More</button>
+
+      <p><b>Total: </b>{videos.length}</p>
+
+      {loading
+        ? "loading.."
+        : videos.map((video, index) => (
+            <div key={index}>
+              <p>
+                <b>#{index}</b> {video.video_title}
+              </p>
+              <p>
+                <b>{video.channel_name}</b>
+              </p>
+              <a href={video.video_url} target="_blank" rel="noreferrer">
+                <button>Play</button>
+              </a>
+              <button onClick={() => postVideo(video)}>Post</button>
+              <button onClick={() => deleteVideo(video)}>Delete</button>
+            </div>
+          ))}
     </div>
   );
 }
-
-export default App;
